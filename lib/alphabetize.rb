d7d176421571abdf91a0ef50d68222780fe6f560
DDarrow123 <ddarrowphl@gmail.com>
2018-09-12 13:26:02 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,33 @@
-def alphabetize(arr)
-  # code here
+require 'pry'
+
+def alphabetize(phrases_array)
+  #phrases_array = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+  new_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+  #["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
+  phrases_array.sort_by do |phrase|
+    
+    phrase.chars.map do |c|
+      new_alphabet.index(c)
+    end
+  end
+  #=> {"a"=>0, "b"=>1, "c"=>2, ..., "v"=>26, "z"=>27}
+
 end
+
+
+
+  #arr.sort do |a, b|
+
+#if a == b
+    #  0
+#    elsif a > b
+  #    1
+  #  elsif a < b
+
+#    -1
+
+  #  end
+
+#  end
+
+#end
